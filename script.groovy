7e549d85ed77e27bf7829236f78c85356a859c7a
aktham333mm <aktham.maayeh@gmail.com>
2024-12-19 12:46:23 +0300

    UPDT

--- script.groovy
+++ script.groovy
@@ -1,9 +1,9 @@
 def  build() {
-    echo "buld app"
+    bat "mvn clean package -Dmaven.test.skip"
 }
 
-def test() {
-    echo "test app"
+def copyfile() {
+    bat "copy C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\workin-springboot_main\\target\\*.jar C:\\test101"
 }
 
 return this //so it can be imported to jenkinsfile
